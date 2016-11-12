5971cd98b9bc09cea40e4b03e502198c6bc9c6db
Yuval Pruss <yuvalp@c4internal.c4-security.com>
2016-11-12 22:40:28 +0200

    Countinue working on the sceleton of the site

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+/* jshint esversion: 6 */
+
+(function(window) {
+	'use strict';
+
+	function getPosts() {
+		//Get the posts
+	}
+
+	$( document ).ready(function() {
+		getPosts();
+	});
+
+	window.app = window.app || {};
+})(window);
